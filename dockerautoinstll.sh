2ecf9c795bd20b042a573da3eedb2347dd4b4790
mack078 <mackwen10@gmail.com>
2020-10-27 11:27:37 +0800

    Update dockerautoinstll.sh
    add firewall setp

--- dockerautoinstll.sh
+++ dockerautoinstll.sh
@@ -2,6 +2,8 @@ yum update
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce-17.12.1.ce
+systemctl stop firewalld
+systemctl disable firewalld
 systemctl start docker
 systemctl enable docker
 yum -y install epel-release
